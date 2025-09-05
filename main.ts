88b251e9bb422adb2b07b4e3f4aac1fda69ee27c
aarav-singhal035 <aarav.singhal@stu.ocsb.ca>
2025-09-05 12:50:48 -0400

    changed my name

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: XXX
- * Created on: Sep 2020
+ * Created on: Sep 2025
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Aarav!')
